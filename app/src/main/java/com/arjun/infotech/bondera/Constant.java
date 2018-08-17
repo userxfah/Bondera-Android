1cc75506139c20abfb443ccfae8dd85e70845237
Billy Everyteen <sakariyaravi1996.sr11@gmail.com>
2018-08-17 18:51:20 +0530

    Push testing

--- app/src/main/java/com/arjun/infotech/bondera/Constant.java
+++ app/src/main/java/com/arjun/infotech/bondera/Constant.java
@@ -1,7 +1,7 @@
 package com.arjun.infotech.bondera;
 
 public class Constant {
-//        public static String API_KEY_GOOGLE_PLACES = "AIzaSyDISb2f-DyKlwkozBfW9R_ebZXFIWSvQG4";
+        public static String API_KEY_GOOGLE_PLACES = "AIzaSyDISb2f-DyKlwkozBfW9R_ebZXFIWSvQG4";
     public static String API_KEY_GOOGLE_PLACES = "AIzaSyAtXizjmXibFeYN5y5NMW2bkSe19y0SSUI";
 //    public static String API_KEY_GOOGLE_PLACES = "AIzaSyCiWTHqrjDqX90ea80CcZ3gM_y38kbqz68";
 
